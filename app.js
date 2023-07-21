57748f27dbc65e32f4188600892f06c07699b872
Michael Hiiva <michaelhiiva@gmail.com>
2023-07-20 20:14:42 -0700

    Basic working application.

---
+++ app.js
@@ -0,0 +1,52 @@
+const buttons = document.querySelectorAll(".ratingButtons button");
+const submit = document.getElementById("submitBtn");
+const error = document.getElementById("errorMsg");
+const selectionMsg = document.getElementById("selectionMsg");
+const box1 = document.getElementById("ratingState");
+const box2 = document.getElementById("selectionResult");
+let selected = "";
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        removeSelectedClasses()
+        selected = e.target.textContent
+        e.target.classList.add("selected")
+    })
+})
+
+submit.addEventListener("click", (e) => {
+    if (selected === "") {
+        addButtonErrors()
+        setTimeout(() => {
+            removeButtonErrors()
+        }, 3000);
+        return
+    }
+
+    selectionMsg.textContent = `You selected ${selected} out of 5`
+    box1.classList.add("display-none");
+    box2.classList.remove("display-none");
+})
+
+function addButtonErrors()
+{
+    error.classList.remove("display-none")
+    buttons.forEach(btn => {
+        btn.classList.add("error")
+    })
+}
+
+function removeButtonErrors()
+{
+    error.classList.add("d-none")
+    buttons.forEach(btn => {
+        btn.classList.remove("error")
+    })
+}
+
+function removeSelectedClasses()
+{
+    buttons.forEach(btn => {
+        btn.classList.remove("selected")
+    })
+}
